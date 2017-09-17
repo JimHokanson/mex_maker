2d788ab3e3f7d780b5ba8556ab6da4b1cd7e0e19
Jim Hokanson <jim.hokanson@gmail.com>
2017-09-17 10:18:50 -0400

    output of mex file is now specified

--- +mex/+build/@main_spec/main_spec.m
+++ +mex/+build/@main_spec/main_spec.m
@@ -2,13 +2,27 @@ classdef main_spec
     %
     %   Class
     %   mex.build.main_spec
+    %
+    %   This is the nearly final version of how the code should be built.
+    %   The entries themselves can be modified manually if needed.
+    %
+    %   Usage approach 1:
+    %   -----------------
+    %   spec = compiler.getBuildSpec()
+    %   %make modifications 
+    %   spec.build()
+    %   
+    %
+    %   Usage approach 2:
+    %   -----------------
+    %   compiler.build %this class isn't seen by the user
+    %   
     
     properties
         cmd
         verbose
-        compiler
-        compile_entries
-        linker_entry
+        compile_entries     %   mex.build.compiler_entry
+        linker_entry        %   mex.build.linker_entry
     end
     
     methods
@@ -29,6 +43,17 @@ classdef main_spec
             cmds = obj.compile_entries.getCompileStatements();
         end
         function build(obj)
+            %
+            %   TODO: Link to known build calls in the compilers (currently
+            %   only gcc only)
+            %   
+            %   See Also
+            %   --------
+            %   mex.build.compiler_entry
+            %   mex.build.linker_entry
+            
+            %TODO: On failure cleanup
+            
             obj.compile_entries.execute();
             obj.linker_entry.execute();
             
